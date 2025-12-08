901af77c448fba072d8225812aa0f523eebe71fd
plpe2 <villanuevapv1@gmail.com>
2025-12-08 19:41:37 +0800

    design: login page

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,61 @@
 "use client";
-import { useState } from "react";
+import { SetStateAction, useState } from "react";
+
+const LoginElement = ({
+  isShown,
+  setShown,
+}: {
+  isShown: boolean;
+  setShown: React.Dispatch<SetStateAction<boolean>>;
+}) => {
+  return (
+    <div
+      style={{
+        backgroundColor: "red",
+        width: "30%",
+        textAlign: "center",
+        padding: "10px",
+      }}
+    >
+      <h2>Login</h2>
+      <form>
+        <p>Email:</p>
+        <input type="text" name="Email" id="" />
+        <p>Password:</p>
+        <input type="text" name="Password" id="" />
+        <div style={{ margin: "10px" }}>
+          <button type="submit">Login</button>
+          <hr />
+          <p>
+            No account yet?{" "}
+            <button
+              type="button"
+              style={{
+                background: "none",
+                border: "none",
+                color: "blue",
+                textDecoration: "underline",
+                cursor: "pointer",
+                padding: 0,
+              }}
+              onClick={() => {
+                setShown((prev) => !prev);
+              }}
+            >
+              Register
+            </button>
+          </p>
+        </div>
+      </form>
+    </div>
+  );
+};
 
 export default function Home() {
-  const [score, setScore] = useState<number>(0);
+  const [isLogin, setStatus] = useState<boolean>(false);
   return (
-    <div>
-      {score}
-      Dashboard <p>Home</p>
-    </div>
+    <>
+      <LoginElement isShown={isLogin} setShown={setStatus} />
+    </>
   );
 }
