44f0bdce573b3daf3056cd67f15790de0f474394
plpe2 <villanuevapv1@gmail.com>
2025-12-08 21:40:43 +0800

    style: Register Page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,89 @@
 "use client";
 import { SetStateAction, useState } from "react";
 
-const LoginElement = ({
+const LoginFields = ({
+  setShown,
+}: {
+  setShown: React.Dispatch<SetStateAction<boolean>>;
+}) => {
+  return (
+    <form>
+      <p>Email:</p>
+      <input type="text" name="Email" />
+      <p>Password:</p>
+      <input type="text" name="Password" />
+      <div style={{ margin: "10px" }}>
+        <button type="submit">Login</button>
+        <hr />
+        <p>
+          No account yet?{" "}
+          <button
+            type="button"
+            style={{
+              background: "none",
+              border: "none",
+              color: "blue",
+              textDecoration: "underline",
+              cursor: "pointer",
+              padding: 0,
+            }}
+            onClick={() => {
+              setShown((prev) => !prev);
+              console.log("Login");
+            }}
+          >
+            Register
+          </button>
+        </p>
+      </div>
+    </form>
+  );
+};
+
+const RegisterFields = ({
+  setShown,
+}: {
+  setShown: React.Dispatch<SetStateAction<boolean>>;
+}) => {
+  return (
+    <form>
+      <p>Name:</p>
+      <input type="text" name="Name" />
+      <p>Age:</p>
+      <input type="text" name="Age" />
+      <p>Password:</p>
+      <input type="text" name="Password" />
+      <p>Confirm Password:</p>
+      <input type="text" name="CPassword" />
+      <div style={{ margin: "10px" }}>
+        <button type="submit">Login</button>
+        <hr />
+        <p>
+          No account yet?{" "}
+          <button
+            type="button"
+            style={{
+              background: "none",
+              border: "none",
+              color: "blue",
+              textDecoration: "underline",
+              cursor: "pointer",
+              padding: 0,
+            }}
+            onClick={() => {
+              setShown((prev) => !prev);
+              console.log("Register");
+            }}
+          >
+            Already have an account?
+          </button>
+        </p>
+      </div>
+    </form>
+  );
+};
+
+const LogRegContainer = ({
   isShown,
   setShown,
 }: {
@@ -15,47 +97,29 @@ const LoginElement = ({
         width: "30%",
         textAlign: "center",
         padding: "10px",
+        display: "block",
       }}
     >
-      <h2>Login</h2>
-      <form>
-        <p>Email:</p>
-        <input type="text" name="Email" id="" />
-        <p>Password:</p>
-        <input type="text" name="Password" id="" />
-        <div style={{ margin: "10px" }}>
-          <button type="submit">Login</button>
-          <hr />
-          <p>
-            No account yet?{" "}
-            <button
-              type="button"
-              style={{
-                background: "none",
-                border: "none",
-                color: "blue",
-                textDecoration: "underline",
-                cursor: "pointer",
-                padding: 0,
-              }}
-              onClick={() => {
-                setShown((prev) => !prev);
-              }}
-            >
-              Register
-            </button>
-          </p>
-        </div>
-      </form>
+      {!isShown ? (
+        <>
+          <h2>Login</h2>
+          <LoginFields setShown={setShown} />
+        </>
+      ) : (
+        <>
+          <h2>Register</h2>
+          <RegisterFields setShown={setShown} />
+        </>
+      )}
     </div>
   );
 };
 
 export default function Home() {
-  const [isLogin, setStatus] = useState<boolean>(false);
+  const [isShown, setStatus] = useState<boolean>(false);
   return (
     <>
-      <LoginElement isShown={isLogin} setShown={setStatus} />
+      <LogRegContainer isShown={isShown} setShown={setStatus} />
     </>
   );
 }
