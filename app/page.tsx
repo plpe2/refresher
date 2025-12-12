d7fc02e20c1f36dbb95bf8cb12a33764821b129f
plpe2 <villanuevapv1@gmail.com>
2025-12-12 23:09:29 +0800

    refactor: Relocation of Login/Register Container into Navigation bar

--- app/page.tsx
+++ app/page.tsx
@@ -1,46 +1,5 @@
 "use client";
-import { LoginFields } from "@/sections/Login/Login";
-import { RegisterFields } from "@/sections/Login/Register";
-import { SetStateAction, useState } from "react";
-
-const LogRegContainer = ({
-  isShown,
-  setShown,
-}: {
-  isShown: boolean;
-  setShown: React.Dispatch<SetStateAction<boolean>>;
-}) => {
-  return (
-    <div
-      style={{
-        backgroundColor: isShown ? "green" : "teal",
-        width: "30%",
-        textAlign: "center",
-        padding: "10px",
-        display: "block",
-        color: "white",
-      }}
-    >
-      {!isShown ? (
-        <>
-          <h2>Login</h2>
-          <LoginFields setShown={setShown} />
-        </>
-      ) : (
-        <>
-          <h2>Register</h2>
-          <RegisterFields setShown={setShown} />
-        </>
-      )}
-    </div>
-  );
-};
 
 export default function Home() {
-  const [isShown, setStatus] = useState<boolean>(false);
-  return (
-    <>
-      <LogRegContainer isShown={isShown} setShown={setStatus} />
-    </>
-  );
+  return <></>;
 }
--- sections/NavigationBar/navbar.tsx
+++ sections/NavigationBar/navbar.tsx
@@ -1,30 +1,70 @@
-const NavbarStyle = {};
+"use client";
+import { SetStateAction, useState } from "react";
+import { LoginFields } from "../Login/Login";
+import { RegisterFields } from "../Login/Register";
 
-export default function navbar() {
+export const LogRegContainer = ({
+  isShown,
+  setShown,
+}: {
+  isShown: boolean;
+  setShown: React.Dispatch<SetStateAction<boolean>>;
+}) => {
   return (
     <div
       style={{
-        backgroundColor: "gray",
-        height: "8vh",
-        marginBottom: "1.5%",
-        borderRadius: "0 0 10px 10px",
-        display: "flex",
-        justifyContent: "center",
+        backgroundColor: isShown ? "green" : "teal",
+        width: "30%",
         textAlign: "center",
+        padding: "10px",
+        display: "block",
         color: "white",
       }}
     >
-      <div style={{ display: "flex", width: "90%" }}>
-        <div style={{ display: "flex" }}>
-          <p>Home</p>
-        </div>
-        <div style={{ flexGrow: 1 }}></div>
-        <div style={{ display: "flex" }}>
-          <ul>About Us</ul>
-          <ul>Contact Us</ul>
-          <ul>Login/Register</ul>
+      {!isShown ? (
+        <>
+          <h2>Login</h2>
+          <LoginFields setShown={setShown} />
+        </>
+      ) : (
+        <>
+          <h2>Register</h2>
+          <RegisterFields setShown={setShown} />
+        </>
+      )}
+    </div>
+  );
+};
+
+export default function Navbar() {
+  const [isShown, setStatus] = useState<boolean>(false);
+  return (
+    <>
+      <div
+        style={{
+          backgroundColor: "gray",
+          height: "8vh",
+          marginBottom: "1.5%",
+          borderRadius: "0 0 10px 10px",
+          display: "flex",
+          justifyContent: "center",
+          textAlign: "center",
+          color: "white",
+        }}
+      >
+        <div style={{ display: "flex", width: "90%" }}>
+          <div style={{ display: "flex" }}>
+            <p>Home</p>
+          </div>
+          <div style={{ flexGrow: 1 }}></div>
+          <div style={{ display: "flex" }}>
+            <ul>About Us</ul>
+            <ul>Contact Us</ul>
+            <ul>Login/Register</ul>
+          </div>
         </div>
       </div>
-    </div>
+      <LogRegContainer isShown={isShown} setShown={setStatus} />
+    </>
   );
 }
