3cad06ee428028bfb0f88e0f44a0fb8c875b3bac
plpe2 <villanuevapv1@gmail.com>
2026-01-31 21:11:27 +0800

    feature: create insert endpoint and datetime display

--- app/api/v1/task/[taskId]/route.ts
+++ app/api/v1/task/[taskId]/route.ts
@@ -1,10 +1,33 @@
+import getConnection from "@/lib/db_connection";
 import { NextResponse } from "next/server";
 
 export async function GET(
   req: Request,
-  { params }: { params: Promise<{ taskId: string }> }
+  { params }: { params: Promise<{ taskId: string }> },
 ) {
   const taskId = (await params).taskId;
 
   return NextResponse.json({ givenId: taskId });
+}
+
+export async function POST(
+  req: Request,
+  { params }: { params: Promise<{ taskId: string }> },
+) {
+  const { title, body } = await req.json();
+  const taskId = (await params).taskId;
+  const conn = await getConnection();
+  const insertTask = await conn.query(
+    "INSERT INTO tasktbl (`title`, `body`, `userId`) VALUES (?,?,?)",
+    [title, body, taskId],
+  );
+
+  if (!insertTask) {
+    return NextResponse.json({ status: false });
+  }
+
+  return NextResponse.json({
+    status: true,
+    taskContent: { title: title, body: body, requestor: taskId },
+  });
 }
--- app/task/page.tsx
+++ app/task/page.tsx
@@ -1,15 +1,39 @@
 "use client";
 
-import { AuthContext } from "@/context/auth-context";
 import { useAuthProvider } from "@/context/jwt/auth-provider";
 import { Task } from "@/types/Tasks";
-import { authPlugins } from "mysql2";
 import { useEffect, useState } from "react";
 
 export default function TaskView() {
   const userData = useAuthProvider();
   const [taskList, setTasks] = useState<Task[]>([]);
   const [isCreating, setStatusCreate] = useState<boolean>(false);
+
+  const handleCreateTask = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const formData = new FormData(e.currentTarget);
+    const taskTitle = formData.get("Title");
+    const taskBody = formData.get("Body");
+
+    const createRequest = await fetch(
+      `http://localhost:3000/api/v1/task/${userData?.user?.id}`,
+      {
+        method: "POST",
+        headers: {
+          "Content-type": "application/json",
+        },
+        body: JSON.stringify({ title: taskTitle, body: taskBody }),
+      },
+    );
+
+    const createResponse = await createRequest.json();
+    if (!createResponse.status) {
+      console.log("Error creating task");
+    }
+
+    window.location.href = "http://localhost:3000/task";
+  };
 
   const createWindow = () => {
     return (
@@ -22,7 +46,7 @@ export default function TaskView() {
           width: "auto",
         }}
       >
-        <form onSubmit={() => alert("submitted")}>
+        <form onSubmit={handleCreateTask}>
           <button
             type="button"
             onClick={() => setStatusCreate((prev) => !prev)}
@@ -31,10 +55,10 @@ export default function TaskView() {
           </button>
           <p>Create Task</p>
           Title:
-          <input type="text" />
+          <input name="Title" type="text" />
           <br />
           Description:
-          <input type="text" />
+          <input name="Body" type="text" />
           <br />
           <button type="submit">Create</button>
         </form>
@@ -87,7 +111,8 @@ export default function TaskView() {
           <h4>{task.title}</h4>
           <p>- {task.body}</p>
           <p>Status: {task.status}</p>
-          <p>Added: {task.timeAdded.toString()}</p>
+          <p>Date Added: {new Date(task.timeAdded).toLocaleDateString()}</p>
+          <p>Time Added: {new Date(task.timeAdded).toLocaleTimeString()}</p>
           <p>Finished: {task.timeFinished.toString()}</p>
         </div>
       ))}
