e064fadbfdd2158848bcf0af5d7e394f9b8730a3
plpe2 <147789736+plpe2@users.noreply.github.com>
2026-02-05 22:26:42 +0800

    feature: created Update window and viewing of taskDetails

--- app/task/page.tsx
+++ app/task/page.tsx
@@ -2,13 +2,28 @@
 
 import { useAuthProvider } from "@/context/jwt/auth-provider";
 import { CreateWindow } from "@/sections/Task/CreateTask";
+import UpdateTask from "@/sections/Task/UpdateTask";
 import { Task } from "@/types/Tasks";
 import { useEffect, useState } from "react";
+
+export type UpdatingTaskType = {
+  taskId: number;
+  taskTitle: string;
+  taskDesc: string;
+  ownerId: number;
+};
 
 export default function TaskView() {
   const userData = useAuthProvider();
   const [taskList, setTasks] = useState<Task[]>([]);
   const [isCreating, setStatusCreate] = useState<boolean>(false);
+  const [isUpdating, setStatusUpdate] = useState<boolean>(false);
+  const [UpdatingTask, setTaskDetails] = useState({
+    taskId: 0,
+    taskTitle: "",
+    taskDesc: "",
+    ownerId: 0,
+  });
 
   const createTask = () => {
     setStatusCreate((prev) => !prev);
@@ -30,6 +45,7 @@ export default function TaskView() {
     };
     fetchingTask();
   }, [userData]);
+
   return (
     <div>
       {isCreating && <CreateWindow setStatusCreate={setStatusCreate} />}
@@ -40,26 +56,49 @@ export default function TaskView() {
       <p>Task View</p>
 
       <p>Status: {isCreating ? "creating task" : "not creating task"}</p>
-      {taskList.map((task) => (
-        <div
-          key={task.taskId}
-          style={{
-            backgroundColor: "gray",
-            display: "inline-block",
-            padding: "10px",
-            border: "1px solid black",
-            margin: "10px",
-            borderRadius: "10px 10px",
-          }}
-        >
-          <h4>{task.taskTitle}</h4>
-          <p>- {task.taskDesc}</p>
-          <p>Status: {task.status}</p>
-          <p>Date Added: {new Date(task.timeAdded).toLocaleDateString()}</p>
-          <p>Time Added: {new Date(task.timeAdded).toLocaleTimeString()}</p>
-          <p>Finished: {task.timeFinished.toString()}</p>
-        </div>
-      ))}
+      {taskList.map((task) => {
+        return (
+          <div
+            key={task.taskId}
+            style={{
+              backgroundColor: "gray",
+              display: "inline-block",
+              padding: "10px",
+              border: "1px solid black",
+              margin: "10px",
+              borderRadius: "10px 10px",
+            }}
+          >
+            <h4>{task.taskTitle}</h4>
+            <p>- {task.taskDesc}</p>
+            <p>Status: {task.status}</p>
+            <p>Date Added: {new Date(task.timeAdded).toLocaleDateString()}</p>
+            <p>Time Added: {new Date(task.timeAdded).toLocaleTimeString()}</p>
+            <p>Finished: {task.timeFinished.toString()}</p>
+            <button
+              style={{ float: "right", padding: "10px" }}
+              onClick={() => {
+                setStatusUpdate((prev) => !prev);
+                setTaskDetails({
+                  ...UpdatingTask,
+                  taskId: task.taskId,
+                  taskTitle: task.taskTitle,
+                  taskDesc: task.taskDesc,
+                  ownerId: task.ownerId,
+                });
+              }}
+            >
+              Update
+            </button>
+          </div>
+        );
+      })}
+      {isUpdating && (
+        <UpdateTask
+          passedTask={UpdatingTask}
+          setStatusUpdate={setStatusUpdate}
+        />
+      )}
     </div>
   );
 }
---
+++ sections/Task/UpdateTask.tsx
@@ -0,0 +1,64 @@
+import { UpdatingTaskType } from "@/app/task/page";
+import { Task } from "@/types/Tasks";
+import React, { SetStateAction, useState } from "react";
+
+export default function UpdateTask({
+  passedTask,
+  setStatusUpdate,
+}: {
+  passedTask: UpdatingTaskType;
+  setStatusUpdate: React.Dispatch<SetStateAction<boolean>>;
+}) {
+  const [taskDetails, setDetails] = useState<UpdatingTaskType>(passedTask);
+  return (
+    <div
+      style={{
+        display: "flex",
+        padding: "100px",
+        margin: "10px",
+        backgroundColor: "red",
+        width: "auto",
+        position: "absolute",
+        top: "50%",
+        left: "50%",
+        transform: "translate(-50%, -50%)",
+      }}
+    >
+      <form>
+        <button
+          onClick={() => {
+            setStatusUpdate((prev) => !prev);
+            setDetails({
+              ...taskDetails,
+              taskId: 0,
+              taskTitle: "",
+              taskDesc: "",
+              ownerId: 0,
+            });
+          }}
+        >
+          x
+        </button>
+        <p>UpdateTask</p>
+        <p>Title: </p>
+        <input
+          type="text"
+          name="newTitle"
+          defaultValue={taskDetails?.taskTitle}
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+            setDetails({ ...taskDetails, taskTitle: e.target.value })
+          }
+        />
+        <p>Body:</p>
+        <input
+          type="text"
+          name="newBody"
+          defaultValue={taskDetails?.taskDesc}
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+            setDetails({ ...taskDetails, taskDesc: e.target.value })
+          }
+        />
+      </form>
+    </div>
+  );
+}
