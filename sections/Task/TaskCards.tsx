a9a2b286cb0d2954327eb4d3360c258050d0e4df
plpe2 <147789736+plpe2@users.noreply.github.com>
2026-02-14 22:50:19 +0800

    refactor: changes Done button into Change Status select input

--- sections/Task/TaskCards.tsx
+++ sections/Task/TaskCards.tsx
@@ -2,6 +2,25 @@ import { UpdatingTaskType, Task } from "@/types/Tasks";
 import React, { SetStateAction, useState } from "react";
 import ValidationWindow from "./ValidationWindow";
 
+const ChangeStatusArea = ({
+  changeConfirming,
+}: {
+  changeConfirming: React.Dispatch<SetStateAction<boolean>>;
+}) => {
+  return (
+    <div style={{ position: "absolute", display: "grid" }}>
+      <button
+        style={{ padding: "10px", width: "137%" }}
+        onClick={() => changeConfirming((prev) => !prev)}
+      >
+        Ongoing
+      </button>
+      <button style={{ padding: "10px", width: "137%" }}>Finished</button>
+      <button style={{ padding: "10px", width: "137%" }}>Cancel</button>
+    </div>
+  );
+};
+
 export default function TaskCards({
   task,
   setStatusUpdate,
@@ -13,7 +32,9 @@ export default function TaskCards({
   UpdatingTask: UpdatingTaskType;
   setTaskDetails: React.Dispatch<SetStateAction<Partial<Task>>>;
 }) {
-  const [DoneTask, setStatus] = useState<boolean>(false);
+  const [isConfirming, changeConfirming] = useState<boolean>(false);
+  const [isStatusChanging, changeDisplayStatus] = useState<boolean>(false);
+
   return (
     <div
       style={{
@@ -31,29 +52,42 @@ export default function TaskCards({
       <p>Date Added: {new Date(task.timeAdded).toLocaleDateString()}</p>
       <p>Time Added: {new Date(task.timeAdded).toLocaleTimeString()}</p>
       <p>Finished: {task.timeFinished.toString()}</p>
-      <button
-        type="button"
-        style={{ float: "right", padding: "10px" }}
-        onClick={() => setStatus((prev) => !prev)}
-      >
-        Done
-      </button>
-      <button
-        style={{ float: "right", padding: "10px" }}
-        onClick={() => {
-          setStatusUpdate((prev) => !prev);
-          setTaskDetails({
-            ...UpdatingTask,
-            taskId: task.taskId,
-            taskTitle: task.taskTitle,
-            taskDesc: task.taskDesc,
-            userId: task.userId,
-          });
-        }}
-      >
-        Update
-      </button>
-      {DoneTask && <ValidationWindow setStatus={setStatus} />}
+      <div style={{ display: "flex" }}>
+        {/* <button
+          type="button"
+          style={{ padding: "10px" }}
+          onClick={() => setStatus((prev) => !prev)}
+        >
+          Done
+        </button> */}
+        <button
+          style={{ padding: "10px" }}
+          onClick={() => {
+            setStatusUpdate((prev) => !prev);
+            setTaskDetails({
+              ...UpdatingTask,
+              taskId: task.taskId,
+              taskTitle: task.taskTitle,
+              taskDesc: task.taskDesc,
+              userId: task.userId,
+            });
+          }}
+        >
+          Update
+        </button>
+        {isConfirming && <ValidationWindow setStatus={changeConfirming} />}
+        <div>
+          <button
+            style={{ padding: "10px" }}
+            onClick={() => changeDisplayStatus((prev) => !prev)}
+          >
+            Change Status
+          </button>
+          {isStatusChanging && (
+            <ChangeStatusArea changeConfirming={changeConfirming} />
+          )}
+        </div>
+      </div>
     </div>
   );
 }
