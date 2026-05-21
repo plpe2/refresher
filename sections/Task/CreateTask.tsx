236f9fc382cf997eb7d5f7517ba764071c9af743
plpe2 <villanuevapv1@gmail.com>
2026-05-21 22:57:21 +0800

    refactor: Refactored function of Create Task, applied zod Validation

--- sections/Task/CreateTask.tsx
+++ sections/Task/CreateTask.tsx
@@ -1,16 +1,30 @@
+"use client";
 import { useAuthProvider } from "@/context/jwt/auth-provider";
 import { handleCreateTask } from "@/hooks/api/task/task";
 import { SetStateAction } from "react";
 import Container from '@mui/material/Container'
 import { Paper, TextField, Typography } from "@mui/material";
+import { string, z } from "zod"
+import { useForm } from "react-hook-form";
+import { useRouter } from "next/navigation";
+
+const taskValSchema = z.object({
+  title: string().min(1, "Enter the Task title."),
+  body: string().min(1, "Enter Task Description."),
+})
+
+export type taskValTypes = z.infer<typeof taskValSchema>
 
 export const CreateWindow = ({
   setStatusCreate,
 }: {
   setStatusCreate: React.Dispatch<SetStateAction<boolean>>;
 }) => {
+  const router = useRouter();
   const userData = useAuthProvider();
   const id = userData?.user?.id;
+  const { register, handleSubmit, formState: { errors } } = useForm<taskValTypes>()
+
   return (
     <Container maxWidth="lg" sx={{
       width: "auto",
@@ -20,12 +34,7 @@ export const CreateWindow = ({
       <Paper elevation={4} sx={{ padding: 4 }}>
         <form
           style={{ display: "flex", flexDirection: "column", gap: 12 }}
-          onSubmit={(e) =>
-            handleCreateTask({
-              e,
-              id: id!,
-            })
-          }
+          onSubmit={handleSubmit(async (data) => handleCreateTask(id!, data, router))}
         >
           <button
             type="button"
@@ -35,8 +44,8 @@ export const CreateWindow = ({
             x
           </button>
           <Typography variant="body1" color="initial">Create Task</Typography>
-          <TextField label="Task Name" variant="outlined" name="Title" fullWidth />
-          <TextField label="Task Description" variant="outlined" name="Body" fullWidth />
+          <TextField label="Task Name" variant="outlined" {...register("title")} error={!!errors.title} helperText={errors.title?.message} fullWidth />
+          <TextField label="Task Description" variant="outlined" {...register("body")} error={!!errors.body} helperText={errors.body?.message} fullWidth />
           <br />
           <br />
           <button type="submit">Create</button>
