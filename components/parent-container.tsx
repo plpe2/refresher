aed21af3d4acff2cd455bf86e34f7be352dff728
plpe2 <villanuevapv1@gmail.com>
2026-06-10 21:55:52 +0800

    chore: added flex grow in the main content

--- components/parent-container.tsx
+++ components/parent-container.tsx
@@ -5,9 +5,9 @@ import Navbar from "@/features/NavigationBar/navbar"
 
 export const ParentContainer = ({ children }: { children: React.ReactNode }) => {
     return (
-        <Box sx={{ display: "flex", bgcolor: "blue", width: "100%" }}>
+        <Box sx={{ display: "flex", bgcolor: "blue", width: "100%", minHeight: "100vh" }}>
             <SideBar />
-            <Box >
+            <Box sx={{ flexGrow: 4 }}>
                 <Navbar />
                 {children}
             </Box>
