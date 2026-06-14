2a192139458e202ae54b2f089f97ad76ceb83d2b
plpe2 <villanuevapv1@gmail.com>
2026-06-14 19:55:45 +0800

    style: added MUI icons and shrink Navbar to accommodate just icons.

--- components/Sidebar/sidebar-navigation.tsx
+++ components/Sidebar/sidebar-navigation.tsx
@@ -1,7 +1,12 @@
-import { Container } from "@mui/material"
+import { Container, Typography } from "@mui/material"
+import Link from "next/link"
 
 export const SideBar = () => {
     return <Container maxWidth="lg" sx={{ width: "10%", bgcolor: "red" }}>
-        Side bar
+        <Typography variant="body1" color="initial">
+            <Link href={"/"}>
+                Todo App
+            </Link>
+        </Typography>
     </Container>
 }
--- features/NavigationBar/navbar.tsx
+++ features/NavigationBar/navbar.tsx
@@ -1,69 +1,57 @@
 "use client";
 import { useContext } from "react";
 
-import Link from "next/link";
 import { AuthContext } from "@/context/auth-context";
+import { Badge, Box, IconButton } from "@mui/material";
+import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
+import NotificationsIcon from '@mui/icons-material/Notifications';
 
 export default function Navbar() {
   const userData = useContext(AuthContext);
 
-  const btnDisplay = () => {
-    if (!userData?.isAuthenticated) {
-      return (
-        <button
-          style={{ padding: "10px" }}
-          type="button"
-        >
-          Login/Register
-        </button>
-      );
-    } else {
-      return (
-        <button
-          style={{ padding: "10px" }}
-          type="button"
-          onClick={userData.logout}
-        >
-          Logout
-        </button>
-      );
-    }
-  };
   return (
     <>
       <div
         style={{
           backgroundColor: "gray",
-          height: "8vh",
+          height: "6vh",
           marginBottom: "1.5%",
           borderRadius: "0 0 10px 10px",
-          display: "flex",
-          justifyContent: "center",
-          textAlign: "center",
-          color: "white",
+          width: "20%",
+          justifySelf: "right",
+          // position: "absolute",
+          padding: "10px",
+          justifyItems: "center"
         }}
       >
-        <div style={{ display: "flex", width: "90%" }}>
-          <div style={{ display: "flex" }}>
-            <Link href={`http://localhost:3000/`}>
-              <p>Home</p>
-            </Link>
-          </div>
-          <div style={{ flexGrow: 1 }}></div>
-          <div style={{ display: "flex" }}>
-            <ul>
-              <button style={{ padding: "10px" }} type="button">
-                About Us
-              </button>
-            </ul>
-            <ul>
-              <button style={{ padding: "10px" }} type="button">
-                Contact Us
-              </button>
-            </ul>
-            <ul>{btnDisplay()}</ul>
-          </div>
-        </div>
+        <Box>
+          <IconButton
+            sx={{
+              width: 40,
+              height: 40,
+              p: 3
+            }}
+            aria-label="show 4 unread messages"
+            onClick={() => console.log("haha1")}
+          >
+            <Badge badgeContent={4} color="primary">
+              <NotificationsIcon />
+            </Badge>
+          </IconButton>
+          <IconButton
+            sx={{
+              width: 40,
+              height: 40,
+              p: 3
+            }}
+            aria-label="show 4 unread messages"
+            onClick={() => userData?.logout()}
+          >
+            <Badge color="primary">
+              <MoreHorizIcon />
+            </Badge>
+          </IconButton>
+        </Box>
       </div>
     </>
   );
