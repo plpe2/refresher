e5956d21f5e95f6095cf000fc9d0fba7f6febe8e
plpe2 <villanuevapv1@gmail.com>
2026-06-20 22:38:24 +0800

    style: Skeletal layout of the Dashboard.

--- features/Dashboard/Dashboard.tsx
+++ features/Dashboard/Dashboard.tsx
@@ -1,11 +1,25 @@
-import { Box } from '@mui/material'
+import { Box, Typography, Container } from '@mui/material'
 import { ParentContainer } from '@/components/parent-container'
 
 export default function Dashboard() {
     return (
         <ParentContainer>
-            <Box sx={{}}>
-                Dashboard
+            <Box sx={{ bgcolor: "beige" }}>
+                <Typography variant="h4" color="initial">Dashboard</Typography>
+                <Container maxWidth="lg" sx={{ border: "1px solid black", height: "30vh", mt: 5, mb: 5 }}>
+                    <Typography variant="body1" color="initial">Task Count</Typography>
+                </Container>
+                <Container sx={{ display: "flex", gap: 2 }}>
+                    <Container maxWidth="lg" sx={{ border: "1px solid black", height: "30vh", mb: 5 }}>
+                        <Typography variant="body1" color="initial">Task Count</Typography>
+                    </Container>
+                    <Container maxWidth="lg" sx={{ border: "1px solid black", height: "30vh", mb: 5 }}>
+                        <Typography variant="body1" color="initial">Task Count</Typography>
+                    </Container>
+                    <Container maxWidth="lg" sx={{ border: "1px solid black", height: "30vh", mb: 5 }}>
+                        <Typography variant="body1" color="initial">Task Count</Typography>
+                    </Container>
+                </Container>
             </Box>
         </ParentContainer>
     )
