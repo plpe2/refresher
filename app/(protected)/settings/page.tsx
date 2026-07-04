80eeac8c633990aa67b4cc13aeb7bb9742dae33a
plpe2 <villanuevapv1@gmail.com>
2026-07-04 12:13:42 +0800

    style: Added containers for other settings. styled buttons and aligned correctly.

--- app/(protected)/settings/page.tsx
+++ app/(protected)/settings/page.tsx
@@ -8,6 +8,7 @@ export default function SettingsPage() {
         <Typography variant="body1" color="initialtial">
             Manage your account preferences and settings
         </Typography>
+        {/* Profile Container */}
         <SettingsContainer title="Profile">
             <SettingsItem title="Full Name" >
                 <Button variant="contained" color="primary">
@@ -25,11 +26,69 @@ export default function SettingsPage() {
                 </Button>
             </SettingsItem>
         </SettingsContainer>
-        <Box>
-            <Button variant="contained" color="primary">
+
+        {/* Notification Container */}
+        <SettingsContainer title="Notification">
+            <SettingsItem title="Full Name" >
+                <Button variant="contained" color="primary">
+                    asd
+                </Button>
+            </SettingsItem>
+            <SettingsItem title="Email" >
+                <Button variant="contained" color="primary">
+                    asd
+                </Button>
+            </SettingsItem>
+            <SettingsItem title="Role" >
+                <Button variant="contained" color="primary">
+                    asd
+                </Button>
+            </SettingsItem>
+        </SettingsContainer>
+
+        {/* Appearance Container */}
+        <SettingsContainer title="Appearance">
+            <SettingsItem title="Full Name" >
+                <Button variant="contained" color="primary">
+                    asd
+                </Button>
+            </SettingsItem>
+            <SettingsItem title="Email" >
+                <Button variant="contained" color="primary">
+                    asd
+                </Button>
+            </SettingsItem>
+            <SettingsItem title="Role" >
+                <Button variant="contained" color="primary">
+                    asd
+                </Button>
+            </SettingsItem>
+        </SettingsContainer>
+
+        {/* Security Container */}
+        <SettingsContainer title="Secutiry">
+            <SettingsItem title="Full Name" >
+                <Button variant="contained" color="primary">
+                    asd
+                </Button>
+            </SettingsItem>
+            <SettingsItem title="Email" >
+                <Button variant="contained" color="primary">
+                    asd
+                </Button>
+            </SettingsItem>
+            <SettingsItem title="Role" >
+                <Button variant="contained" color="primary">
+                    asd
+                </Button>
+            </SettingsItem>
+        </SettingsContainer>
+
+        <Box sx={{ width: "90%", padding: "10px", display: "flex", gap: 3 }}>
+            <Button variant="contained" color="primary" sx={{ width: "70%" }}>
                 Save Changes
             </Button>
-            <Button variant="contained" color="primary">
+            <Button variant="contained" color="error" sx={{ width: "30%" }}>
                 Cancel
             </Button>
         </Box>
